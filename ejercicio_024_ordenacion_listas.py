fcc9f9e40ed5ef0e08e03f3c11965d0c7dd6e063
lafabricadeandroides <fernando.paniagua.python@gmail.com>
2025-02-03 16:46:17 +0100

    Update ejercicio_024_ordenacion_listas.py

--- ejercicio_024_ordenacion_listas.py
+++ ejercicio_024_ordenacion_listas.py
@@ -15,6 +15,26 @@ lista_ordenada = sorted(lista) # Indicando reverse=True invierte el orden
 print('Lista:', lista)
 print('Lista ordenada:', lista_ordenada) # Lista ordenada: ['Invierno', 'Primavera', 'Verano', 'otoño'] (otoño va al final porque la o minúscula va después de las mayúsculas en las tablas de caracteres)
 
-
+# Las listas de números las ordena correctamente
 lista_numeros = [100000,38,10,88,3234234]
 print(sorted(lista_numeros)) # [10, 38, 88, 100000, 3234234]
+
+# Ordenar una lista de tuplas (ordena por el primer elemento)
+lista_tuplas = [(3,8),(10,1),(-4,5)]
+lista_tuplas_ordenadas = sorted(lista_tuplas)
+print(lista_tuplas_ordenadas) # [(-4, 5), (3, 8), (10, 1)]
+
+# El argumento key
+def valoracion(elemento):
+    return elemento[1] # Segundo elemento de cada tupla
+
+lista_tuplas_ordenadas = sorted(lista_tuplas, key=valoracion)
+print(lista_tuplas_ordenadas) # [(10, 1), (-4, 5), (3, 8)]
+
+lista_tuplas.sort(key=valoracion)
+print(lista_tuplas) # [(10, 1), (-4, 5), (3, 8)]
+
+
+
+
+

e89712880129861bb5969a14905204f545bdd59b
lafabricadeandroides <fernando.paniagua.python@gmail.com>
2025-02-03 17:38:59 +0100

    Ejercicios

--- ejercicio_026_ordenacion_listas_bis_bis.py
+++ ejercicio_026_ordenacion_listas_bis_bis.py
@@ -3,9 +3,27 @@ vehiculos = [('Seat',25_000,120), ('Fiat',15_000,80), ('Skoda',25_000,200), ('Ni
 # Utilizar la función sorted
 
 # 1. Ordenar por precio de menor a mayor
-
+vehiculos.sort(key=lambda vehiculo : vehiculo[1])
+print('Test 1:',vehiculos)
 
 # 2. Ordenar por precio de menor a mayor y (en caso de igualdad) por potencia de mayor a menor
+def ponderar(vehiculo):
+    return vehiculo[1],-vehiculo[2] # Devolución de una tupla
 
+vehiculos.sort(key=ponderar)
+print('Test 2:',vehiculos)
 
 # 3. Ordenar por potencia de mayor a menor
+def valorar_vehiculo(vehiculo):
+    return vehiculo[2]
+
+vehiculos.sort(key=valorar_vehiculo, reverse=True)
+print('Test 3:',vehiculos)
+
+# Solución Víctor
+# vehiculos = [ ('Fiat',15_000,80), ('Skoda',25_000,200), ('Nissan',18_000,180), ('Seat',25_000,120)]
+# vehiculos.sort(key=lambda vehiculo: vehiculo[2], reverse=True)
+# def valorar_precio(vehiculo):
+#     return vehiculo[1]
+# vehiculos.sort(key=valorar_precio, reverse=False)
+# print(vehiculos)
---
+++ ejercicio_028_lambda.py
@@ -0,0 +1,6 @@
+def sumar(s1, s2):
+    resultado = s1+s2
+    return resultado
+
+s = lambda s1, s2 : s1 + s2
+print(s(3,8))

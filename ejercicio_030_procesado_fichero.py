f57beeae40e85656b8ed974efbff02399231ab74
lafabricadeandroides <fernando.paniagua.python@gmail.com>
2025-02-03 20:35:03 +0100

    Sesión 06

--- ejercicio_030_procesado_fichero.py
+++ ejercicio_030_procesado_fichero.py
@@ -1,7 +1,5 @@
 #https://babel.upm.es/~angel/teaching/pps/quijote.txt
 
-#0. Obtener una palabra
-palabra = input('Introduce una palabra:')
 
 # 1. Leer el fichero y guardar el contenido en un variable
 with open('quijote.txt',encoding='utf-8',mode='rt') as fichero:
@@ -9,9 +7,42 @@ with open('quijote.txt',encoding='utf-8',mode='rt') as fichero:
     #print(texto[-100:]) #Muestra los 100 últimos caracteres
 
 # 2. Dada una palabra, averiguar si está o no está en el texto.
+palabra = input('Introduce una palabra:')
+existe = palabra.upper() in texto.upper()
+print(f'La palabra {palabra} {"existe" if existe else "NO existe"} en el texto')
+
+# Solución con index
+# palabra = input('Introduce una palabra:')
+# try:
+#     posicion = texto.upper().index(palabra.upper())
+#     print(f'La palabra {palabra} existe en el texto')
+# except ValueError as ve:
+#     print(f'La palabra {palabra} NO existe en el texto')
 
 # 3. Dada una palabra, averiguar cuantas veces está en el texto.
+palabra = input('Introduce una palabra:')
+numero_apareciones = texto.upper().count(palabra.upper())
+print(numero_apareciones)
+
+# Solución con expresiones regulares
+# import re
+# apariciones= re.findall(palabra, texto, re.IGNORECASE)
+# print(len(apariciones)) # 283 (apariciones una lista con las palabras originales que hacen 'match' con la cadena busqueda)
 
 # 4. Generar un nuevo fichero en el que se cambien los nombres de los personajes.
+personaje = input('Introduce nombre del personaje:')
+personaje_reemplazo = input('Introduce nombre del personaje de reemplazo:')
+import re
+apariciones= re.findall(palabra, texto, re.IGNORECASE)
+conjunto_apariciones = set(apariciones)
+nuevo_texto = texto
+for aparicion in conjunto_apariciones:
+    if (aparicion.isupper()):
+        nuevo_texto = nuevo_texto.replace(aparicion, personaje_reemplazo.upper())
+    else:
+        nuevo_texto = nuevo_texto.replace(aparicion, personaje_reemplazo)
+
+with open('quijote_modificado.txt',encoding='utf-8',mode='wt') as fichero:
+    fichero.write(nuevo_texto)
 
 # Ojo con las mayúsculas y minúsculas.
